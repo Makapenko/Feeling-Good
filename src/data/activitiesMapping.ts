700e903098fbb17aa678cfb3bea1f56ea79f212f
Makapenko <makap88@gmail.com>
2025-03-14 21:45:47 +0300

    fix: tasks now in right chapters

--- src/data/activitiesMapping.ts
+++ src/data/activitiesMapping.ts
@@ -26,24 +26,25 @@ export const allActivities: SpecialContent[] = [
 
 // Маппинг подглав к активностям, которые они открывают
 export const chapterToActivitiesMap: Record<string, SpecialContent[]> = {
-  'ch2': ['burns-checklist'], // Оставляем, так как это одиночная глава
+  'ch2': ['burns-checklist'],
   'ch3-1': ['cognitive-biases', 'cognitive-biases-test'],
-  'ch4-1': ['three-columns-method'],
-  'ch4-2': ['thought-diary'],
+  'ch4-2': ['three-columns-method', 'thought-diary'],
   'ch5-1': ['daily-schedule'],
   'ch5-2': ['anti-procrastination'],
-  'ch5-3': ['thought-diary'],
   'ch5-4': ['pleasure-sheet'],
   'ch5-5': ['no-buts'],
   'ch5-6': ['self-support'],
   'ch5-7': ['hindering-helping-thoughts'],
   'ch5-8': ['small-steps'],
   'ch5-9': ['motivation-without-coercion'],
-  'ch6-2': ['disarming-technique'],
+  'ch5-10': ['disarming-technique'],
+  'ch5-11': ['imagine-success'],
+  'ch5-12': ['count-achievements'],
+  'ch5-13': ['check-cant-do'],
+  'ch5-14': ['no-lose-technique'],
+  'ch5-15': ['self-activation',],
   'ch7-1': ['novaco-scale'],
-  'ch13-2': ['imagine-success', 'count-achievements'],
-  'ch13-3': ['check-cant-do'],
-  'ch13-4': ['no-lose-technique']
+
 };
 
 // Получить все активности, доступные для данного набора глав
