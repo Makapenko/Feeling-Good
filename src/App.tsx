471071fc4361eba4d14d9305fe4311739101da9c
Makapenko <makap88@gmail.com>
2025-07-05 20:00:33 +0300

    feat: add chapter 7 documentation and remove legacy migration files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import styles from './App.module.css';
 import Header from './components/Header/Header';
 import ListOfChapters from './components/ListOfChapters/ListOfChapters';
@@ -9,16 +8,11 @@ import NotificationContainer from './components/Notification/NotificationContain
 import UnlockNotifier from './components/Notification/UnlockNotifier';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import { migrateDataToRedux } from './redux/migrateLegacyData';
 import { useIsMobile } from './utils/deviceUtils';
-// TODO: Задания из введений не отображаются в ActivitiesPanel
+
 function App() {
   const isMobile = useIsMobile();
 
-  useEffect(() => {
-    // Запускаем миграцию данных
-    migrateDataToRedux();
-  }, []);
 
   return (
     <Provider store={store}>
