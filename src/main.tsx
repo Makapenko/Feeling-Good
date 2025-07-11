5ae3be94cd7b73ebdd26836c592640114e373fa3
Makapenko <makap88@gmail.com>
2025-07-11 14:04:49 +0300

    feat: add @stagewise/toolbar integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,16 @@
+import { initToolbar } from '@stagewise/toolbar';
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const stagewiseConfig = {
+  plugins: [],
+};
+
+if (import.meta.env.DEV) {
+  initToolbar(stagewiseConfig);
+}
 
 createRoot(document.getElementById('root')!).render(
     <App />
